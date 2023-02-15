632ed3ae13eb642ed8dbbd6a37a899cb0268d0e0
Marcos <marcos.ruiz@enti.cat>
2023-02-15 21:35:16 +0100

    LLaves

--- Assets/Scripts/Doors/DoorsBehaivour.cs
+++ Assets/Scripts/Doors/DoorsBehaivour.cs
@@ -4,18 +4,64 @@ using UnityEngine;
 
 public class DoorsBehaivour : MonoBehaviour
 {
+    
+    private enum DoorType { GREEN, RED, BLUE};
+    [SerializeField]
+    DoorType type;
+    [SerializeField]
     private BoxCollider2D door;
     private Animator doorAnimator;
+    private SpriteRenderer spriteRenderer;
     private void Awake()
     {
-        door= GetComponent<BoxCollider2D>(); 
-        doorAnimator = GetComponent<Animator>();    
+        doorAnimator = GetComponent<Animator>();   
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+    private void Start()
+    {
+        switch (type)
+        {
+            case DoorType.GREEN:
+                spriteRenderer.color = Color.green;
+                break;
+            case DoorType.RED:
+                spriteRenderer.color = Color.red;
+                break;
+            case DoorType.BLUE:
+                spriteRenderer.color = Color.blue;
+                break;
+            default:
+                break;
+        }
+    }
+    private void CheckCanOpen()
+    {
+        switch (type)
+        {
+            case DoorType.GREEN:
+                
+                break;
+            case DoorType.RED:
+         
+                break;
+            case DoorType.BLUE:
+          
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void DoorOpen()
+    {
+        doorAnimator.SetBool("isDeath", true);
+        door.enabled = false;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            doorAnimator.SetBool("isDeath", true);
+            CheckCanOpen();
         }
     }
 }
