b84df925ff39fa9da737d95e7d2ba638d931285c
Daniel7802 <daniel.munoz.jimenez@enti.cat>
2023-02-17 20:53:04 +0100

    Ataque melee y prefab del player hechos

---
+++ Assets/Scripts/Player/PlayerMeleeAttack.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMeleeAttack : MonoBehaviour
+{
+    private SpriteRenderer _sp;
+    private Animator _anim;
+    private bool startCoolDown;
+    private float coolDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sp = GetComponent<SpriteRenderer>();
+        _anim = GetComponent<Animator>();
+        coolDown = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.G) && startCoolDown == false)
+        {
+            _anim.SetBool("Attack", true);
+            startCoolDown = true;
+        }     
+
+        if(startCoolDown == true)
+        {
+            coolDown--;
+        }
+
+        if(coolDown <= 0)
+        {
+            startCoolDown = false;
+            _anim.SetBool("Attack", false);
+            coolDown = 50;
+        }
+    }
+}
